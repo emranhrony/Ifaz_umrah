6d67fbf19927c301f48139107cdecc7fb43c0438
emranhrony <emranhusseninbd@gmail.com>
2025-09-28 07:57:49 -0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Ifaz_umrah/',
-  build: {
-    outDir: 'docs'
-  }
+  build: { outDir: 'docs' }
 })
