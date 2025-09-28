2bf39220d9a0dda4edb3b9fcb7f99930fa3653d7
emranhrony <emranhusseninbd@gmail.com>
2025-09-28 20:41:31 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Ifaz_umrah/',   // 👉 আপনার repo নাম এখানে দিতে হবে (case-sensitive)
+  base: '/Ifaz_umrah/',
   build: {
     outDir: 'docs'
   }
